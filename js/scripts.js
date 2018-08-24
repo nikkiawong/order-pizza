4a76569023333e4f92954f7f6db1bcd5530e101f
Nikki Wong <nikkiwong@miami.edu>
2018-08-24 11:29:02 -0700

    calculate total cost using size property in constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 //business logic
-function Pizza(toppings, size) {
-  this.toppings = toppings;
+function Pizza(topping, size) {
+  this.topping = topping;
   this.size = size;
 }
-
-// function Toppings(pepperoni, sausage, mushrooms, onion, pepper, pineapple) {
+//
+// function topping(pepperoni, sausage, mushrooms, onion, pepper, pineapple) {
 //   this.pepperoni = pepperoni;
 //   this.sausage = sausage;
 //   this.mushrooms = mushrooms;
@@ -12,36 +12,35 @@ function Pizza(toppings, size) {
 //   this.pepper = pepper;
 //   this.pineapple = pineapple;
 // }
-//
-// function size(small, medium, large, family) {
-//   this.small = 5;
-//   this.medium = 10;
-//   this.large = 15;
-//   this.family = 20;
-// }
 
-Pizza.prototype.calculateCost = function(numberOfToppings, size) {
-    var total = (numberOfToppings * 0.5) + (size * 5);
-    console.log(size);
+Pizza.prototype.calculateCost = function(numberOfToppings) {
+    var total = (numberOfToppings * 0.5) + (this.size * 5);
     return total;
 }
+//
+// Pizza.prototype.calculateCostOfSize = function() {
+//   var total = this.size * 5;
+//   return total;
+// }
 
 //user interface logic
 $(document).ready(function() {
-  var onePizza = new Pizza(Pizza.toppings, Pizza.size);
 
   $("form#pizzaForm").submit(function(event) {
     event.preventDefault();
     $("#orderConfirmation").show();
 
-    var toppingsInput = $("#toppingsSelect").val();
-    var numberOfToppings = toppingsInput.length;
-    var size = parseFloat($("#sizeSelect").val());
-    // var sizeCost = (sizeInput * 5);
-    var totalCost = onePizza.calculateCost(numberOfToppings, size);
+    var toppingInput = $("#toppingsSelect").val();
+    var numberOfToppings = toppingInput.length;
+    var sizeInput = $("#sizeSelect").val();
 
-    $("#displayToppings").text(toppingsInput);
-    // $("#displaySize").text();
-    $("#finalCost").text(totalCost);
+    var onePizza = new Pizza(numberOfToppings, sizeInput);
+
+    var totalCost = onePizza.calculateCost(numberOfToppings);
+    console.log("total is $" + totalCost);
+
+    $("#displayToppings").text("$" + (numberOfToppings * 0.5).toFixed(2));
+    $("#displaySize").text("$" + (sizeInput * 5).toFixed(2));
+    $("#finalCost").text(totalCost.toFixed(2));
   });
 });
